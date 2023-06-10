a1a2f5fed9b0a65d926ef229b35dab4dbc2aa390
WZS-Juditost <wzswang@ucdavis.edu>
2023-06-09 23:02:33 -0700

    change portal setting

--- Survival/Assets/Scripts/TeleportCaptain.cs
+++ Survival/Assets/Scripts/TeleportCaptain.cs
@@ -25,7 +25,7 @@ public class TeleportCaptain : MonoBehaviour
 
         if(collision.gameObject.name == "Main Character")
         {
-            collision.gameObject.transform.position = new Vector2(Random.value * 20.0f - 10.0f, this.DestinationLevel.transform.position.y + 4);
+            collision.gameObject.transform.position = new Vector2(Random.value * 10.0f - 30.0f, this.DestinationLevel.transform.position.y + 4);
             this.MainCamera.transform.position = new Vector3(this.DestinationLevel.transform.position.x, this.DestinationLevel.transform.position.y, this.MainCamera.transform.position.z);
             FindObjectOfType<SoundManager>().PlaySoundEffect("bomb");
         }
